f7d754c79428189e8423ff8c0d847bece6ff09cc
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2023-07-19 23:30:09 +0200

    Dialogue Editor Open only dialog object

--- Assets/Dialogue/Editor/DialogueEditor.cs
+++ Assets/Dialogue/Editor/DialogueEditor.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.Callbacks;
 
 namespace FPS.Dialogue.Editor
 {
@@ -11,6 +12,18 @@ namespace FPS.Dialogue.Editor
         public static void ShowEditorWindow()
          {
             GetWindow(typeof(DialogueEditor), false, "Dialog Editor");
+
+        }
+
+        [OnOpenAssetAttribute(1)]
+        public static bool OnOpenAsset(int instanceID, int line)
+        {
+            Dialogue dialogue = EditorUtility.InstanceIDToObject(instanceID) as Dialogue;
+            if (dialogue != null) {
+                ShowEditorWindow();
+                return true;
+            } 
+            return false;
 
         }
     }
